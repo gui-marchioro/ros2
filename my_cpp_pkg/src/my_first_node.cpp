e50c970792b377c547920a7ddb63596d2a350e62
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-09-04 12:02:02 -0300

    Tranformed cpp node into a class

--- my_cpp_pkg/src/my_first_node.cpp
+++ my_cpp_pkg/src/my_first_node.cpp
@@ -1,12 +1,32 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyFirstNode : public rclcpp::Node
+{
+public:
+    MyFirstNode() : Node("my_first_node"), m_count(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello, ROS2 from C++");
+        m_timer = this->create_wall_timer(
+            std::chrono::seconds(1),
+            std::bind(&MyFirstNode::TimerCallback, this)
+        );
+    }
+private:
+    void TimerCallback()
+    {
+        m_count++;
+        RCLCPP_INFO(this->get_logger(), "Timer callback count: %d", m_count);
+    }
+    rclcpp::TimerBase::SharedPtr m_timer;
+    int m_count;
+};
+
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
 
-    auto node = std::make_shared<rclcpp::Node>("my_first_node");
-    RCLCPP_INFO(node->get_logger(), "Hello, ROS2 from C++17");
-    
+    auto node = std::make_shared<MyFirstNode>();
+
     rclcpp::spin(node);
     
     rclcpp::shutdown();
