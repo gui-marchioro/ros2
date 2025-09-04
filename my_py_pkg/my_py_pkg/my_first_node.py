28a3237cddaf416db43a5306f11c2d10f905ee50
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-09-04 11:20:20 -0300

    Added timer callback to the python node

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -6,7 +6,13 @@ from rclpy.node import Node
 class MyFirstNode(Node):
     def __init__(self):
         super().__init__("my_first_node")
+        self._counter = 0
         self.get_logger().info("Hello ROS2")
+        self.timer = self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self._counter += 1
+        self.get_logger().info(f"Timer callback triggered. Count: {self._counter}")
 
 
 def main(args=None):
