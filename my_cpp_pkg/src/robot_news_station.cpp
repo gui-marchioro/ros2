dca5e5eefc4956f40a636f0110c075d90ff709d9
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-09-09 22:37:47 -0300

    Added cpp publisher sample

---
+++ my_cpp_pkg/src/robot_news_station.cpp
@@ -0,0 +1,35 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+
+class RobotNewsStationNode : public rclcpp::Node
+{
+public:
+    RobotNewsStationNode() : Node("robot_news")
+    {
+        m_robot_name = "C3PO";
+        m_publisher = this->create_publisher<example_interfaces::msg::String>("robot_news", 10);
+        m_timer = this->create_wall_timer(std::chrono::seconds(1), std::bind(&RobotNewsStationNode::publish_news, this));
+        RCLCPP_INFO(this->get_logger(), "Robot News Station has been started");
+    }
+
+private:
+    std::string m_robot_name;
+    rclcpp::Publisher<example_interfaces::msg::String>::SharedPtr m_publisher;
+    rclcpp::TimerBase::SharedPtr m_timer;
+
+    void publish_news()
+    {
+        auto message = example_interfaces::msg::String();
+        message.data = std::string("Hello from ") + m_robot_name + std::string(" robot");
+        m_publisher->publish(message);
+    }
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<RobotNewsStationNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
