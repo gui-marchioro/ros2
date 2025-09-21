cd561f494f00d20a3d17b8e4acee2de8e8ba3ef9
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-09-21 15:53:52 -0300

    Added service to reset counter in the number counter sample

--- my_cpp_pkg/src/activity_02_03/number_counter.cpp
+++ my_cpp_pkg/src/activity_02_03/number_counter.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
 
 class NumberCounter : public rclcpp::Node
 {
@@ -9,6 +10,9 @@ public:
         m_publisher = this->create_publisher<example_interfaces::msg::Int64>("number_count", 10);
         m_subscription = this->create_subscription<example_interfaces::msg::Int64>(
             "number", 10, std::bind(&NumberCounter::CallbackNumber, this, std::placeholders::_1));
+        m_service = this->create_service<example_interfaces::srv::SetBool>(
+            "reset_counter",
+            std::bind(&NumberCounter::ResetCounter, this, std::placeholders::_1, std::placeholders::_2));
         RCLCPP_INFO(this->get_logger(), "Number counter node has been started");
         RCLCPP_INFO(this->get_logger(), "Counter value: '%ld'", m_count);
     }
@@ -17,6 +21,7 @@ private:
     int64_t m_count = 0;
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr m_subscription;
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr m_publisher;
+    rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr m_service;
 
     void CallbackNumber(const example_interfaces::msg::Int64::SharedPtr msg)
     {
@@ -31,6 +36,18 @@ private:
         auto message = example_interfaces::msg::Int64();
         message.data = count;
         m_publisher->publish(message);
+    }
+
+    void ResetCounter(const std::shared_ptr<example_interfaces::srv::SetBool::Request> request,
+                        std::shared_ptr<example_interfaces::srv::SetBool::Response> response)
+    {
+        std::string message = request->data ? "Counter reset to 0" : "Counter not reset";
+        RCLCPP_INFO(this->get_logger(), message.c_str());
+        request->data ? m_count = 0 : m_count = m_count;
+        RCLCPP_INFO(this->get_logger(), "Counter value: '%ld'", m_count);
+        PublishCount(m_count);
+        response->success = true;
+        response->message = message;
     }
 };
 
