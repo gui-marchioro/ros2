2f66923059c57f31f4df180381ac417f00310468
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-09-27 16:54:32 -0300

    Added callback to change parameters at runtime

--- my_cpp_pkg/src/activity_02_03/number_publisher.cpp
+++ my_cpp_pkg/src/activity_02_03/number_publisher.cpp
@@ -13,6 +13,7 @@ public:
         m_publisher = this->create_publisher<example_interfaces::msg::Int64>("number", 10);
         m_timer = this->create_wall_timer(std::chrono::duration<double>(time_period), std::bind(&NumberPublisherNode::publish_number, this));
         RCLCPP_INFO(this->get_logger(), "Number Publisher has been started");
+        this->add_post_set_parameters_callback(std::bind(&NumberPublisherNode::parameter_callback, this, std::placeholders::_1));
     }
 
 private:
@@ -25,6 +26,25 @@ private:
         auto message = example_interfaces::msg::Int64();
         message.data = m_number;
         m_publisher->publish(message);
+    }
+
+    void parameter_callback(const std::vector<rclcpp::Parameter> & parameters)
+    {
+        for (const auto & parameter : parameters)
+        {
+            if (parameter.get_name() == "number")
+            {
+                m_number = parameter.as_int();
+                RCLCPP_INFO(this->get_logger(), "Number parameter changed to: %d", m_number);
+            }
+            else if (parameter.get_name() == "time_period")
+            {
+                double time_period = parameter.as_double();
+                m_timer->cancel();
+                m_timer = this->create_wall_timer(std::chrono::duration<double>(time_period), std::bind(&NumberPublisherNode::publish_number, this));
+                RCLCPP_INFO(this->get_logger(), "Time period parameter changed to: %f seconds", time_period);
+            }
+        }
     }
 };
 
--- my_py_pkg/my_py_pkg/activity_02_03/number_publisher.py
+++ my_py_pkg/my_py_pkg/activity_02_03/number_publisher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 from example_interfaces.msg import Int64
 
 
@@ -14,11 +15,23 @@ class NumberPublisherNode(Node):
         self._number_publisher = self.create_publisher(Int64, "number", 10)
         self._number_timer = self.create_timer(self._time_period, self.publish_number)
         self.get_logger().info("Number publisher has been started.")
+        self.add_post_set_parameters_callback(self.parameter_callback)
 
     def publish_number(self):
         msg = Int64()
         msg.data = self._number
         self._number_publisher.publish(msg)
+
+    def parameter_callback(self, params: list[Parameter]):
+        for param in params:
+            if param.name == "number" and param.type_ == param.Type.INTEGER:
+                self._number = param.value
+                self.get_logger().info(f"Parameter 'number' changed to {self._number}")
+            elif param.name == "time_period" and param.type_ == param.Type.DOUBLE:
+                self._time_period = param.get_parameter_value().double_value
+                self._number_timer.cancel()
+                self._number_timer = self.create_timer(self._time_period, self.publish_number)
+                self.get_logger().info(f"Parameter 'time_period' changed to {self._time_period}")
 
 
 def main(args=None):
