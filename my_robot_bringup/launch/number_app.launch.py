497a1edea82d5a7eda8763e3365bace3c258833e
Guilherme Marchioro <gui02_marchioro@hotmail.com>
2025-09-28 19:17:56 -0300

    Added remappimgs in launch file

--- my_robot_bringup/launch/number_app.launch.py
+++ my_robot_bringup/launch/number_app.launch.py
@@ -1,18 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
     ld = LaunchDescription()
 
     number_publisher = Node(
         package="my_py_pkg",
         executable="number_publisher",
+        name="my_number_publisher",
+        remappings=[("number", "my_number")],
     )
 
     number_counter = Node(
         package="my_cpp_pkg",
         executable="number_counter",
+        name="my_number_counter",
+        remappings=[("number", "my_number")]
     )
 
     ld.add_action(number_publisher)
